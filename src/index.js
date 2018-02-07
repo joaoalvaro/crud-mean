b98128743eee6e1cc52bff18eb486cb4efea7b59
joaoalvaro <joao.alvarocordeiro@icloud.com>
2018-02-07 02:16:14 -0200

    Creating constant for path file routes.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const indexRoutes = require('./routes/routes');
 
 //Settings
 app.set('port', process.env.PORT || 3000);
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extend: false}));
 
 //Routes
-app.use(require('./routes/routes'));
+app.use(indexRoutes);
 
 app.listen(app.get('port'), () => {
     console.log("Servidor rodando na " + app.get('port'))
