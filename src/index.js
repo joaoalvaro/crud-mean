0811fd7914f4558f680d943fc35664564020b17c
joaoalvaro <joao.alvarocordeiro@icloud.com>
2018-02-07 01:51:55 -0200

    First Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extend: false}));
+
+//Routes
+
+
+
+app.listen(app.get('port'), () => {
+    console.log("Servidor rodando na " + app.get('port'))
+});
