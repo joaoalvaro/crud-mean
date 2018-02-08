8f34073c9f7731272016f3f9a0c3427840c3b316
joaoalvaro <joao.alvarocordeiro@icloud.com>
2018-02-08 18:26:32 -0200

    New updates.

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from 'app/components/cars/cars.component';
+import { CarsService } from 'app/services/cars.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CarsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
