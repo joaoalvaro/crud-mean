9e1948c22ade52e13644205b70de6a584fd50861
joaoalvaro <joao.alvarocordeiro@icloud.com>
2018-02-16 03:44:11 -0200

    Updating module for new components.

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 import { CarsComponent } from 'app/components/cars/cars.component';
 import { CarsService } from 'app/services/cars.service';
-import { HttpClientModule } from '@angular/common/http';
+import { CreateUpdateComponent } from './components/create-update/create-update.component';
+import { ListComponent } from './components/list/list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+    { path: '', component:ListComponent },
+    { path: 'api/createUpdate', component:CreateUpdateComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent
+    CarsComponent,
+    CreateUpdateComponent,
+    ListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    CarsService
+    CarsService,
   ],
   bootstrap: [AppComponent]
 })
