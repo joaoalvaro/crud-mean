73ab80c0ad288d0d700fe070ce2fecea9a5a67c6
joaoalvaro <joao.alvarocordeiro@icloud.com>
2018-02-16 03:49:08 -0200

    Update for new abordading of bd.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const appRoutes = require('./routes/appRoutes');
 
-const indexRoutes = require('./routes/appRoutes');
+mongoose.connect('mongodb://localhost/crud-mean-db');
+
+
 
 //Settings
 app.set('views', path.join(__dirname, 'views'));
@@ -15,11 +19,10 @@ app.set('view engine', 'ejs');
 //Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extend: false}));
+app.use(express.urlencoded({extend: true}));
 
 //Routes
-//app.use(indexRoutes);
-app.use('/api', indexRoutes);
+app.use('/api', appRoutes);
 
 //Static files
 app.use(express.static(path.join(__dirname, 'dist')));
