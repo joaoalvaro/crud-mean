058d0ad8641870958b1bdc9fec84dfadd3f96b3f
joaoalvaro <joao.alvarocordeiro@icloud.com>
2018-02-16 03:33:31 -0200

    Updating router file.

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,12 +1,63 @@
-const router = require('express').Router();
-const mongojs = require('mongojs');
+const express = require('express');
+const router = express.Router();
+var Car = require('../models/dataSchema');
 
-const db = mongojs('crud-mean-db', ['cars']);
-
-router.get('/', (req, res, next) => {
-    res.render('index.html');
+router.post('/create', (req, res, next) => {
+    var newCar = new Car ({
+        brand: req.body.brand,
+        model: req.body.model,
+        color: req.body.color
+    });
+    newCar.save((err, car) => {
+        if (err) {
+            res.status(500).json({ errmsg: err });
+        } else {
+            res.status(200).json({ msg: car });
+        }
+    });
 });
 
+router.get('/read', (req, res, next) => {
+    Car.find({}, (err, cars) => {
+        if (err) {
+            res.status(500).json({ errmsg: err });
+        } else {
+            res.status(200).json({ msg: cars });
+        }
+    });
+});
+
+router.put('/update', (req, res, next) => {
+    Car.findById(req.body._id, (err, car) => {
+        if (err) {
+            res.status(500).json({ errmsg: err });
+        }
+        car.brand = req.body.brand;
+        car.model = req.body.model;
+        car.color = req.body.color;
+        car.save((err, car) => {
+            if (err) {
+                res.status(500).json({ errmsg: err });
+            } else {
+                res.status(200).json({ msg: car });
+            }
+        });
+    });
+});
+
+router.delete('/delete/:id', (req, res, next) => {
+    Car.findOneAndRemove({_id: req.params.id}, (err, car) => {
+        if (err) {
+            res.status(500).json({ errmsg: err });
+        } else {
+            res.status(200).json({ msg: car });
+        }
+    });
+});
+
+module.exports = router;
+
+/*
 router.get('/cars', (req, res, next) => {
     db.cars.find((err, cars) => {
         if (err) {
@@ -18,7 +69,7 @@ router.get('/cars', (req, res, next) => {
 });
 
 router.get('/cars/:id', (req, res, next) => {
-    db.cars.findOne({ _id:  mongojs.ObjectId( req.params.id ) }, (err, car) => {
+    db.cars.findOne({ _id: mongojs.ObjectId(req.params.id) }, (err, car) => {
         if (err) {
             return next(err);
         } else {
@@ -27,7 +78,7 @@ router.get('/cars/:id', (req, res, next) => {
     });
 });
 
-router.post('/cars', (req, res, next) => {
+router.post('/create', (req, res, next) => {
     const car = req.body;
     if( !car.brand || !(car.idDone + '')) {
         res.status(400).json({
@@ -44,9 +95,11 @@ router.post('/cars', (req, res, next) => {
     }
 });
 
-router.delete('/cars/:id', (req, res, next) => {
-    db.cars.remove({ _id: mongojs.ObjectId( req.params.id )}, (err, result) => {
-        if(err) {
+
+
+router.delete('/delete/:id', (req, res, next) => {
+    db.cars.remove({ _id: mongojs.ObjectId(req.params.id) }, (err, result) => {
+        if (err) {
             res.next(err);
         } else {
             res.json(result);
@@ -54,19 +107,19 @@ router.delete('/cars/:id', (req, res, next) => {
     });
 });
 
-router.put('/cars/:id', (req, res, next) => {
+router.put('/update/:id', (req, res, next) => {
     const car = db.body;
     const updateCar = {};
 
-    if(car.brand) {
+    if (car.brand) {
         updateCar.brand = car.brand;
     }
 
-    if(car.model) {
+    if (car.model) {
         updateCar.model = car.model;
     }
 
-    if(car.color) {
+    if (car.color) {
         updateCar.color = car.color;
     }
 
@@ -75,19 +128,13 @@ router.put('/cars/:id', (req, res, next) => {
             error: 'Sem dados para atualizar.'
         });
     } else {
-        db.cars.update({ _id: mongojs.ObjectId(req.params.id)}, updateCar, (err, car) => {
-            if(err) {
+        db.cars.update({ _id: mongojs.ObjectId(req.params.id) }, updateCar, (err, car) => {
+            if (err) {
                 return next(err);
             } else {
                 res.json(car);
             }
         });
     }
-
-    
 });
-
-
-
-
-module.exports = router;
+*/
